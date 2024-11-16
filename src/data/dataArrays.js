6eb80373edada2bb807749cf00712f7d5591f717
kmkmadusanka <kmkasunmadushanka@gmail.com>
2024-11-16 14:52:29 +1100

    Initial commit

---
+++ src/data/dataArrays.js
@@ -0,0 +1,141 @@
+export const categories = [
+  {
+    id: 3,
+    name: "Urban and Cultural Experiences",
+    photo_url:
+      "https://upload.wikimedia.org/wikipedia/commons/6/67/Sydney_Opera_House_Australia.jpg",
+  },
+  {
+    id: 1,
+    name: "Natural Wonders and Landscapes",
+    photo_url:
+      "https://i.pinimg.com/originals/03/65/7f/03657f89ca1fa71df55e164c35afd7e5.jpg",
+  },
+  {
+    id: 2,
+    name: "Beaches and Coastal Areas",
+    photo_url:
+      "https://cdn.zmescience.com/wp-content/uploads/2017/06/Amazing_Great_Barrier_Reef_1.jpg",
+  },
+  {
+    id: 4,
+    name: "Wildlife Experiences",
+    photo_url: "https://i.redd.it/pdian7v6nm331.jpg",
+  },
+];
+
+export const recipes = [
+  {
+    recipeId: 1,
+    categoryId: 1,
+    title: "Great Barrier Reef",
+    photo_url:
+      "https://cdn.zmescience.com/wp-content/uploads/2017/06/Amazing_Great_Barrier_Reef_1.jpg",
+    photosArray: [
+      "https://cdn.zmescience.com/wp-content/uploads/2017/06/Amazing_Great_Barrier_Reef_1.jpg",
+    ],
+    time: "2,300 KM",
+    ingredients: [],
+    description:
+      "The Great Barrier Reef is the world's largest coral reef system, teeming with marine life and offering unparalleled snorkeling and diving experiences.",
+  },
+  {
+    recipeId: 2,
+    categoryId: 1,
+    title: "Blue Mountains",
+    photo_url: "https://peakvisor.com/img/news/Australia-Blue-Mountains.jpg",
+    photosArray: [
+      "https://peakvisor.com/img/news/Australia-Blue-Mountains.jpg",
+    ],
+    time: "900 KM",
+    ingredients: [],
+    description:
+      "Known for its dramatic cliffs, eucalyptus forests, and the iconic Three Sisters rock formation, it's a haven for hikers and nature lovers.",
+  },
+  {
+    recipeId: 3,
+    categoryId: 2,
+    title: "Bondi Beach",
+    photo_url:
+      "https://media.cntraveler.com/photos/546bcacd2a3d21fa285c7c2b/master/pass/australia-bondi-beach-tout.jpg",
+    photosArray: [
+      "https://media.cntraveler.com/photos/546bcacd2a3d21fa285c7c2b/master/pass/australia-bondi-beach-tout.jpg",
+    ],
+    time: "1,000 km",
+    ingredients: [],
+    description:
+      "One of Australia's most famous beaches, Bondi is renowned for its surf culture, golden sands, and vibrant atmosphere.",
+  },
+  {
+    recipeId: 4,
+    categoryId: 2,
+    title: "Gold Coast Beaches",
+    photo_url:
+      "https://www.qantas.com/content/dam/travelinsider/images/explore/australia/queensland/gold-coast/best-beaches-on-the-gold-coast/0fb922ce-91fc-401c-adef-e0682831c8de.jpg",
+    photosArray: [
+      "https://www.qantas.com/content/dam/travelinsider/images/explore/australia/queensland/gold-coast/best-beaches-on-the-gold-coast/0fb922ce-91fc-401c-adef-e0682831c8de.jpg",
+    ],
+    time: "1,700 km",
+    ingredients: [],
+    description:
+      "A series of stunning beaches known for surfing, vibrant nightlife, and family-friendly attractions.",
+  },
+  {
+    recipeId: 5,
+    categoryId: 3,
+    title: "Sydney",
+    photo_url:
+      "https://upload.wikimedia.org/wikipedia/commons/6/67/Sydney_Opera_House_Australia.jpg",
+    photosArray: [
+      "https://upload.wikimedia.org/wikipedia/commons/6/67/Sydney_Opera_House_Australia.jpg",
+    ],
+    time: "900 km",
+    ingredients: [],
+    description:
+      "Australia's largest city, known for its iconic landmarks, bustling harbor, and diverse cultural scene.",
+  },
+  {
+    recipeId: 6,
+    categoryId: 3,
+    title: "Brisbane",
+    photo_url:
+      "https://www.2gb.com/wp-content/uploads/sites/2/2020/04/iStock-175381409.jpg?resize=600%2C400",
+    photosArray: [
+      "https://www.2gb.com/wp-content/uploads/sites/2/2020/04/iStock-175381409.jpg?resize=600%2C400",
+    ],
+    time: "1,700 km",
+    ingredients: [],
+    description:
+      "A subtropical city offering a relaxed lifestyle, outdoor activities, and a growing arts scene.",
+  },
+  {
+    recipeId: 7,
+    categoryId: 4,
+    title: "Kangaroo Island",
+    photo_url:
+      "https://beyondwords.life/wp-content/uploads/2018/01/north-coast-kangaroo-island-australia-900x700.jpg",
+    photosArray: [
+      "https://beyondwords.life/wp-content/uploads/2018/01/north-coast-kangaroo-island-australia-900x700.jpg",
+    ],
+    time: "1,000 km",
+    ingredients: [],
+    description:
+      "A haven for wildlife enthusiasts, offering close encounters with kangaroos, koalas, and sea lions.",
+  },
+  {
+    recipeId: 8,
+    categoryId: 4,
+    title: "Buffalo Pizza",
+    photo_url:
+      "https://images.squarespace-cdn.com/content/v1/596b2969d2b85786e6892853/ba25d17e-8e29-4f23-aeb9-907029cebdaa/Penguin%2BParade%2Bon%2BPhillip%2BIsland",
+    photosArray: [
+      "https://images.squarespace-cdn.com/content/v1/596b2969d2b85786e6892853/ba25d17e-8e29-4f23-aeb9-907029cebdaa/Penguin%2BParade%2Bon%2BPhillip%2BIsland",
+    ],
+    time: "350 km",
+    ingredients: [],
+    description:
+      "Little penguins waddling ashore during the evening Penguin Parade.",
+  },
+];
+
+export const ingredients = [];
